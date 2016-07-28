eae9a7c63378dd5716fe38f497e39ed67c667f7a
jitsejan <jitsejan@gmail.com>
2016-07-28 18:35:28 -0400

    Working base

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
+from django_markdown.admin import MarkdownModelAdmin
 from django.contrib import admin
+from django_markdown.widgets import AdminMarkdownWidget
+from django.db.models import TextField
+from . import models
 
-# Register your models here.
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ("title", "created")
+    prepopulated_fields = {"slug": ("title",)}
+    formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
+
+admin.site.register(models.Entry, EntryAdmin)
+admin.site.register(models.Tag)
--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_markdown',
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
